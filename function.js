c7b3fa4a1b3d41bd45cdc1d61a626f6bd8c9522e
apoorva.ce <apoorva.ce@subex.com>
2024-04-28 22:46:37 +0530

    added additional illustration

--- function.js
+++ function.js
@@ -12,7 +12,6 @@ let language = "Java"+"script";
 dataRemoved[data.language] ="Removed successfully";
 
 //This behavior is part of the flexibility and dynamic nature of the language. When you assign a property to a function, JavaScript treats the function as an object, allowing you to add properties to it
-
 "###############################################################################################################################################################"
 
 
@@ -28,6 +27,44 @@ function printAbovefunction(){
 }
 
 printAbovefunction();
+"###############################################################################################################################################################"
+
+function noParams(){}
+console.log(noParams.length);//0 because no parameter
 
 
+
+function params(a,b,...rest){}
+console.log(params.length);//2 because length don't include rest parameter
+"###############################################################################################################################################################"
+
+
+
+function doTask(){
+    return {
+        success:true
+    };
+}
+console.log(doTask()); //{success:true}
+
+function doTask2(){
+    return
+    {
+        success:true
+    };
+}
+console.log(doTask2()); //undefined because we see the next of return is empty which the browser considers as end of line and adds (;) hence return; is undefined . "Automatic semicolon insertion"
+
+"###############################################################################################################################################################"
+
+function showTask(){
+    this.names = "name"
+    var name="do not create its declaration";
+    console.log(name);
+    console.log(this.names);
+}
+const task1 = new showTask();//new creates a new object reference of showTask function and assigns it to task1 since within function we have used this.name the name is available within the new object and is accessible when we print task1.name it prints with no error
+console.log(task1.name);//undefined because name only exists within showTask() function and do not exist within the new object reference created using "new" keyword
+console.log(task1.names);//"name" but here since we used this.name it creates variable in the new object reference and is accessible
+console.log(task1);
 "###############################################################################################################################################################"
